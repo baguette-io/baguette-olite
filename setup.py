73af869b28d984689b11c2a65483a868765fdbaf
David Macro <david.macro7@gmail.com>
2017-07-21 23:18:37 +0200

    update the setup

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
+"""
+Setup for baguette-olite.
+"""
 from setuptools import setup, find_packages
 
-
-requires = ['sh==1.09', 'Unipath==1.0', 'argparse==1.2.1',
-                        'async==0.6.1', 'coverage==3.7.1', 'gitdb==0.5.4',
-                        'mock==1.0.1', 'nose==1.3.3', 'smmap==0.8.2',
-                        'spec==0.11.1', 'wsgiref==0.1.2']
-
-
 setup(name="baguette-olite",
-            version="1.0.0",
-            platforms='any',
-            packages=find_packages(),
-            include_package_data=True,
-            install_requires=requires,
-            author="Vlad Temian",
-            url="https://github.com/baguette-io/pyolite",
-            description="Python wrapper for gitolite",
-            classifiers=[
-                    'Development Status :: 5 - Production/Stable',
-                    'Intended Audience :: Developers',
-                    'Intended Audience :: System Administrators',
-                    'License :: OSI Approved :: BSD License',
-                    'Operating System :: MacOS :: MacOS X',
-                    'Operating System :: Microsoft :: Windows',
-                    'Operating System :: POSIX',
-                    'Topic :: System :: Networking',
-                    'Programming Language :: Python :: 2.7',
-            ])
+      version="1.0.0",
+      platforms='any',
+      packages=find_packages(),
+      include_package_data=True,
+      author="Vlad Temian",
+      url="https://github.com/baguette-io/pyolite",
+      description="Python wrapper for gitolite",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Topic :: System :: Networking',
+          'Programming Language :: Python :: 2.7',
+      ],
+      install_requires=[
+          'sh==1.09',
+          'Unipath==1.0',
+          'argparse==1.2.1',
+          'async==0.6.1',
+          'coverage==3.7.1',
+          'gitdb==0.5.4',
+          'smmap==0.8.2',
+      ],
+      extras_require={
+          'testing':[
+              'pytest==3.1.3',
+              'mock==1.0.1',
+              'spec==0.11.1',
+          ],
+      },
+     )
