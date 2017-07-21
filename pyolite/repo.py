0cd05e2f9555acc58386b1976a9114a92add18d1
David Macro <david.macro7@gmail.com>
2017-07-21 23:53:08 +0200

    repo: lock file when reading/writing it

--- pyolite/repo.py
+++ pyolite/repo.py
@@ -1,4 +1,5 @@
 import re
+import fcntl
 
 class Repo(object):
     def __init__(self, path):
@@ -6,11 +7,15 @@ class Repo(object):
 
     def replace(self, pattern, string):
         with open(str(self.path), 'r+') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            #
             content = f.read()
             content = re.sub(pattern, string, content)
+            #
             f.seek(0)
             f.write(content)
             f.truncate()
+            fcntl.flock(f, fcntl.LOCK_UN)
 
     @property
     def users(self):
@@ -19,7 +24,9 @@ class Repo(object):
 
         users = []
         with open(str(self.path)) as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             config = f.read()
+            fcntl.flock(f, fcntl.LOCK_UN)
             for match in re.compile('=( *)(\w+)').finditer(config):
                 users.append(match.group(2))
 
@@ -32,11 +39,21 @@ class Repo(object):
 
         groups = []
         with open(str(self.path)) as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             config = f.read()
+            fcntl.flock(f, fcntl.LOCK_UN)
             for match in re.compile('=( *)(@\w+)').finditer(config):
                 groups.append(match.group(2))
         return groups
 
     def write(self, string):
         with open(self.path, 'a') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
             f.write(string)
+            fcntl.flock(f, fcntl.LOCK_UN)
+
+    def overwrite(self, string):
+        with open(self.path, 'w') as f:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            f.write(string)
+            fcntl.flock(f, fcntl.LOCK_UN)
